a63f47ee3654673f1df71ce7647cc7a4a817eee9
Ekrem1999 <61759520+Ekrem1999@users.noreply.github.com>
2020-05-28 11:25:49 +0300

    Update READY-Hasskell.hs

--- READY-Hasskell.hs
+++ READY-Hasskell.hs
@@ -1,10 +1,9 @@
-import Data.List
--- 1
--- Определите функцию, возвращающую последний элемент списка.
---
-
-_last l = if null (tail l) then head l else _last (tail l)
-
+-- №1
+-- Определите функцию, возвращающую последний элемент списка
+ 
+myLast [x] = x
+myLast (_:xs) = myLast xs
+ 
 --5
 --Условие: Определите функцию, упаковывающую последовательные дубликаты списка
 --в подсписки вида (M N), где N - элемент списка, M - количество повторений. Например, [’a’, ’a’, ’a’, ’a’, ’b’, ’c’, ’c’, ’a’, ’a’, ’d’, ’e’,
@@ -13,24 +12,25 @@ _last l = if null (tail l) then head l else _last (tail l)
 encode [] = []
 encode (x:xs) = (length $ x : takeWhile (==x) xs, x)
                  : encode (dropWhile (==x) xs)
-                 
-                                            
+ 
 -- №12
 -- Определите функцию, разбивающую список (a b с d...) на пары ((а b) (с d)...).
 split [] = [] 
 split [x] = [(x,x)] 
 split (x1:x2:xs) = (x1,x2) : (split xs)
-
-main = do
-    putStrLn "Test №1"
+ 
+ 
+ 
+main = do 
+       putStrLn "Task №1"
        print (myLast [1,2,3,4,5])
-       
-       
+ 
        putStrLn "Тest №5"
        print (encode ['a', 'a', 'a', 'b', 'b'])
        print (encode ['a', 'a', 'b', 'b', 'b', 'c', 'c'])
        print (encode ['a', 'a', 'a', 'b', 'b', 'b', 'c', 'c', 'c', 'a'])
-       
+ 
        putStrLn "Test №12"
        print (split ["a","b","c","d","e","f","g"] ) 
-       print (split [1,2,3,4,5] )
+       print (split [1,2,3,4,5] ) 
+ 
